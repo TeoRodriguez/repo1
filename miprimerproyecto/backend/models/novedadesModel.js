899d9c0076054293f45393b546fd419b3785d3e1
Teo Rodriguez Bompas <rodriguez.teo.191@gmail.com>
2022-08-16 08:15:49 -0300

    M5U1

---
+++ miprimerproyecto/backend/models/novedadesModel.js
@@ -0,0 +1,23 @@
+const { isAsyncFunction } = require("util/types");
+var pool = require("./bd");
+
+async function getNovedades() {
+    var query = "select * from novedades order by id DESC ";
+    var rows = await pool.query(query);
+    return rows;
+
+}
+async function insertNovedad(obj){
+    try{
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+    
+    } catch (error){
+        console.log(error);
+        throw error;
+
+    }
+}
+
+module.exports = { getNovedades, insertNovedad }
