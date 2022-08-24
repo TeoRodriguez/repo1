78f910fbe867015d9eff34667385d95bea37c467
Teo Rodriguez Bompas <rodriguez.teo.191@gmail.com>
2022-08-24 11:12:10 -0300

    M6U2

--- miprimerproyecto/backend/routes/admin/novedades.js
+++ miprimerproyecto/backend/routes/admin/novedades.js
@@ -47,4 +47,53 @@ router.post("/agregar", async (req, res, next) => {
 }
 })
 
+// para eliminar novedades
+
+router.get("/eliminar/:id", async (req, res, next) =>{
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect("/admin/novedades");
+})
+
+// para listar una sola novedad por id
+
+router.get ("/modificar/:id", async (req, res, next) =>{
+  var id = req.params.id
+  console.log(req.params.id)
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  console.log(req.params.id)
+  res.render("admin/modificar", {
+    layout: "admin/layout",
+    novedad
+  })
+});
+
+// la que hace todo el proceso
+
+router.post("/modificar", async(req, res, next)=>{
+  try{
+
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    console.log(obj)
+
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect("/admin/novedades");
+
+
+  } catch(error){
+    console.log(error)
+    res.render("admin/modificar",{
+      layout: "admin/layout",
+      error: true,
+      message: "No se modifico la novedad"
+
+    })
+ }
+});
+
 module.exports = router;
