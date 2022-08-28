7602fbdb7e1d8fa9d0490ae7b21419cfb952dc32
Teo Rodriguez Bompas <rodriguez.teo.191@gmail.com>
2022-08-28 12:46:18 -0300

    proyecto

--- miprimerproyecto/backend/routes/api.js
+++ miprimerproyecto/backend/routes/api.js
@@ -2,32 +2,61 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require("./../models/novedadesModel");
 var cloudinary = require("cloudinary").v2; //version de cloudinary//
+var nodemailer = require("nodemailer");
 
 
 router.get("/novedades", async function (req, res, next) {
 
-    let novedades = await novedadesModel.getNovedades();
-  
-    novedades = novedades.map(novedades => {
-      if (novedades.img_id) {
-        const imagen = cloudinary.url(novedades.img_id, {
-          width: 960,
-          height: 200,
-          crop: "fill"
-        });
-        return {
-          ...novedades,
-          imagen
-        }
-      } else {
-        return {
-          ...novedades,
-          imagen: ""
-        }
-      }
-    });
+  let novedades = await novedadesModel.getNovedades();
 
-    res.json(novedades);
+  novedades = novedades.map(novedades => {
+    if (novedades.img_id) {
+      const imagen = cloudinary.url(novedades.img_id, {
+        width: 960,
+        height: 200,
+        crop: "fill"
+      });
+      return {
+        ...novedades,
+        imagen
+      }
+    } else {
+      return {
+        ...novedades,
+        imagen: ""
+      }
+    }
+  });
+
+  res.json(novedades);
+});
+
+
+router.post("/contacto", async (req, res) => {
+  const mail = {
+    to: "rodriguez.teo.191@gmail.com",
+    subject: "Contacto Web",
+    html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} <br> Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+  }
+
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+
+  }); //cierra transport//
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error: false,
+    message: "Mensaje enviado"
+
+  });
+
 });
 
 module.exports = router;
