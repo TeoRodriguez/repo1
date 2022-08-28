2ea8157ee699ab74be42f822cbddf15728087565
Teo Rodriguez Bompas <rodriguez.teo.191@gmail.com>
2022-08-28 20:07:25 -0300

    proyecto1

--- miprimerproyecto/backend/routes/api.js
+++ miprimerproyecto/backend/routes/api.js
@@ -30,4 +30,32 @@ router.get("/novedades", async function (req, res, next) {
     res.json(novedades);
 });
 
+router.post("/contacto", async (req, res) => {
+  const mail = {
+    to: "rodriguez.teo.191@gmail.com",
+    subject: "Contacto Web",
+    html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} <br> Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+  }
+ 
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+ 
+  }); //cierra transport//
+ 
+  await transport.sendMail(mail)
+ 
+  res.status(201).json({
+    error: false,
+    message: "Mensaje enviado"
+ 
+  });
+ 
+});
+
+
 module.exports = router;
